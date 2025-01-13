fa34e1ff4541a6a5c3092996cfcff1a50da5a1bd
suraj maurya <mnaveen1888@gmail.com>
2025-01-13 16:51:28 +0530

    fix name

--- Home.py
+++ Home.py
@@ -73,7 +73,7 @@ def drawLogin():
     with cols[0]:
         pass
     with cols[1]:
-            st.title("Phloton Dashboard Login", anchor=False)
+            st.title("Dashboard Login", anchor=False)
             username_inp = st.text_input("Email").strip()
             password_inp = st.text_input("Password", type="password").strip()
             submit_button = st.button(label="Submit")
